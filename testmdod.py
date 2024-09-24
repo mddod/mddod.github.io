8bb0552507f2d733bba75dfd3fbfe1dd83079870
mddod <164993979+mddod@users.noreply.github.com>
2024-09-24 14:59:42 +0800

    Update testmdod.py

--- testmdod.py
+++ testmdod.py
@@ -3,7 +3,9 @@ import mdod
 
 localFile = 'TestDataset.txt'
 dets= np.loadtxt(localFile,delimiter=',')
+# nd: value of the observation point in the new dimension
 nd = 1
+# sn: number of statistics on the first few numbers in the order of scores from large to small
 sn = 10
 result = mdod.md(dets,nd,sn)
 print (result)
